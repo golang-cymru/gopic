394fabec699ed06c399caecbb98b9c782a66e8aa
Benjamin Misell <benjamin@misell.cymru>
2018-02-22 20:50:18 +0000

    Save uploaded files

--- main.go
+++ main.go
@@ -5,16 +5,38 @@ import (
 	"os"
 	"github.com/nlopes/slack"
 	"context"
+	"log"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	bot := slackbot.New(os.Getenv("SLACK_KEY"))
 
-	toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
+	toMe := bot.Messages(slackbot.DirectMention, slackbot.DirectMessage).Subrouter()
 	toMe.Hear("(?)").MessageHandler(HelloHandler)
 	bot.Run()
 }
 
 func HelloHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
-	bot.Reply(evt, "Oh hello!", true)
+	if evt.Upload {
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", evt.File.URLPrivate, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SLACK_KEY")))
+		res, err := client.Do(req)
+		if err != nil{
+			log.Fatal(err)
+		}
+
+		bytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		ioutil.WriteFile("upload.jpg", bytes, 0755)
+	}
 }
