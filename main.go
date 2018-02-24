fcb06d5f928501deb6e192f9fcf9e41cbbd542fe
Benjamin Misell <benjamin@misell.cymru>
2018-02-24 14:22:53 +0000

    Tidy up a bit

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"io/ioutil"
 	"image/color"
 	"path"
+	"io"
 )
 
 var classifier gocv.CascadeClassifier
@@ -35,46 +36,80 @@ func main() {
 	bot.Run()
 }
 
+func getFile(file *slack.File) (io.Reader, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", file.URLPrivate, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SLACK_KEY")))
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return res.Body, nil
+}
+
+func readImage(reader io.Reader) (image.Image, error) {
+	buf, _ := ioutil.ReadAll(reader)
+	rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+	rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
+
+	var src image.Image
+	_, format, err := image.DecodeConfig(rdr1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	switch format {
+	case "png":
+		src, err = png.Decode(rdr2)
+	case "jpeg":
+		src, err = jpeg.Decode(rdr2)
+	case "gif":
+		src, err = gif.Decode(rdr2)
+	case "bmp":
+		src, err = bmp.Decode(rdr2)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return src, nil
+}
+
+func imageToBmp(image image.Image) (*bytes.Buffer, error) {
+	bmpBuf := new(bytes.Buffer)
+	err := bmp.Encode(bmpBuf, image)
+	if err != nil {
+		return nil, err
+	}
+	return bmpBuf, nil
+}
+
+func imageToJpeg(image image.Image) (*bytes.Buffer, error) {
+	bmpBuf := new(bytes.Buffer)
+	err := jpeg.Encode(bmpBuf, image, &jpeg.Options{Quality:95})
+	if err != nil {
+		return nil, err
+	}
+	return bmpBuf, nil
+}
+
 func HelloHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEvent) {
 	if evt.Upload {
-		client := &http.Client{}
-		req, err := http.NewRequest("GET", evt.File.URLPrivate, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("SLACK_KEY")))
-		res, err := client.Do(req)
+		imgBytes, err := getFile(evt.File)
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
-		buf, _ := ioutil.ReadAll(res.Body)
-		rdr1 := ioutil.NopCloser(bytes.NewBuffer(buf))
-		rdr2 := ioutil.NopCloser(bytes.NewBuffer(buf))
-
-		var src image.Image
-		_, format, err := image.DecodeConfig(rdr1)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		switch format {
-		case "png":
-			src, err = png.Decode(rdr2)
-		case "jpeg":
-			src, err = jpeg.Decode(rdr2)
-		case "gif":
-			src, err = gif.Decode(rdr2)
-		}
-
+		img, err := readImage(imgBytes)
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
-		bmpIn := new(bytes.Buffer)
-		err = bmp.Encode(bmpIn, src)
+		bmpIn, err := imageToBmp(img)
 		if err != nil {
 			log.Println(err)
 			return
@@ -101,14 +136,13 @@ func HelloHandler(ctx context.Context, bot *slackbot.Bot, evt *slack.MessageEven
 		}
 		outBmpBuf := bytes.NewBuffer(outBmp)
 
-		outImg, err := bmp.Decode(outBmpBuf)
+		outImg, err := readImage(outBmpBuf)
 		if err != nil {
 			log.Println(err)
 			return
 		}
 
-		jpegOut := new(bytes.Buffer)
-		err = jpeg.Encode(jpegOut, outImg, &jpeg.Options{Quality:95})
+		jpegOut, err := imageToJpeg(outImg)
 		if err != nil {
 			log.Println(err)
 			return
